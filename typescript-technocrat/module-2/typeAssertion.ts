d15a623635deccd8926241682f35c6bd068ad4e3
rakib8680 <rakib.cst.1st@gmail.com>
2023-11-06 17:51:00 +0600

    type assertion

---
+++ typescript-technocrat/module-2/typeAssertion.ts
@@ -0,0 +1,30 @@
+{
+
+
+    // Type Assertion 
+
+    const kgToGm = (value: string | number): string | number | undefined => {
+        if (typeof value === 'string') {
+            const convertedValue = parseFloat(value) * 1000;
+            return (`The converted value is ${convertedValue} gm`);
+        }
+        else if (typeof value === 'number') {
+            return value * 1000;
+        }
+    };
+
+    const result1 = kgToGm("100") as string
+    const result = kgToGm(1000) as string
+    console.log(result1);
+    console.log(result);
+
+
+
+
+
+
+
+
+
+
+}
