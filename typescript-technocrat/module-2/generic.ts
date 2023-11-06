8fe37679684431babfc48ec2c6102fac3bc3cc3b
rakib8680 <rakib.cst.1st@gmail.com>
2023-11-06 22:36:58 +0600

    generic type

---
+++ typescript-technocrat/module-2/generic.ts
@@ -0,0 +1,65 @@
+
+
+{
+
+
+    // Generic Type .............................................
+
+
+    // Array generic types 
+    // const rollNum : number[] = [4,6,2,7]; 
+    const rollNum: Array<number> = [4, 6, 2, 7]; // Generic Type
+
+    // const admins: string[] = ["John", "Doe", "Jane"];
+    const admins: Array<string> = ["John", "Doe", "Jane"]; // Generic Type
+
+    // const boolean: boolean[] = [true, false, true];
+    const boolean: Array<boolean> = [true, false, true]; // Generic Type
+
+
+
+
+
+    // DYNAMIC Generic Type ............................................
+    type GenericType<T> = Array<T>
+
+    const rollNum2: GenericType<number> = [4, 6, 2, 7];
+    const admins2: GenericType<string> = ["John", "Doe", "Jane"];
+    const boolean2: GenericType<boolean> = [true, false, true];
+
+
+
+    // object using generic type ............................................
+
+    const teacher: GenericType<{ name: string; age: number; role?: string }> = [
+        {
+            name: "John",
+            age: 30,
+        },
+        {
+            name: "Doe",
+            age: 40,
+        },
+        {
+            name: "Jane",
+            age: 50,
+            role: "Admin"
+        }
+    ];
+
+
+    // generic tuple type ................................. ...........
+
+    type GenericTuple<X, Y> = [X, Y];
+
+    const human: GenericTuple<string, string> = ['rakib', 'robin'];
+
+    const userWithId: GenericTuple<number, { name: string, age: number }> = [343, { name: 'jihan', age: 30 }];
+
+
+
+
+
+
+
+}
