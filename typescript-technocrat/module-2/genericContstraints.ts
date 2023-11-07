5e234881fb161bcfd4d9b788af0d1bda86be15f0
rakib8680 <rakib.cst.1st@gmail.com>
2023-11-07 13:50:11 +0600

    generic constraint with type of operator

--- typescript-technocrat/module-2/genericContstraints.ts
+++ typescript-technocrat/module-2/genericContstraints.ts
@@ -11,6 +11,32 @@
     type Owner = "bike" | "car" | "bus";  //manually declare the type
 
     type Owner2 = keyof vehicle; //keyof operator declare the type
-    const person1 : Owner2 = "car";
+    const person1: Owner2 = "car";
+
+
+
+
+    // generic constraint with extends keyword 
+    const getPropertyValue = <X, Y extends keyof X>(obj: X, key: Y) => {
+        return obj[key];
+    }
+
+    const person = {
+        name: "rakib",
+        age: 30,
+        role: "admin"
+    }
+    const vehicle2 = {
+        bike: "yamaha",
+        car: "toyota",
+        bus: "volvo"
+    }
+
+    const result = getPropertyValue(person, "age");
+    const result2 = getPropertyValue(vehicle2, "car");
+
+
+
+
 
 }
