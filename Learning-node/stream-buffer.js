b19820a9fc03c5d9515837db61111f228752f064
rakib8680 <rakib.cst.1st@gmail.com>
2023-11-16 15:36:27 +0600

    created server using raw node.js

---
+++ Learning-node/stream-buffer.js
@@ -0,0 +1,27 @@
+
+const http = require('http');
+const fs = require('fs');
+
+// creating a server using raw node.js 
+const server = http.createServer();
+
+// listener 
+server.on('request', (req, res) => {
+    if (req.url === '/read-file' && req.method === 'GET') {
+        const readableStream = fs.createReadStream('./texts/read.txt');
+        readableStream.on('data', (buffer) => {
+            res.write(buffer);
+        })
+
+        readableStream.on('end', () => {
+
+            res.end('Hello World');
+        })
+    }
+});
+
+
+
+server.listen(5000, () => {
+    console.log('Server is listening on port 5000');
+})
