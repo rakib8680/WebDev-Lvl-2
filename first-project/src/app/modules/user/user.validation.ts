167e09c7757cbeb626b8546182a42dcc2b28da7a
rakib8680 <rakib.cst.1st@gmail.com>
2023-11-26 18:45:42 +0600

    user schema validation complete

---
+++ first-project/src/app/modules/user/user.validation.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const UserValidationSchema = z.object({
+  id: z.string(),
+  password: z
+    .string()
+    .max(20, { message: 'Password must be less than 20 characters' }),
+  needsPasswordChange: z.boolean().default(true).optional(),
+  role: z.enum(['admin', 'student', 'faculty']).default('student'),
+  status: z.enum(['in-progress', 'blocked']).default('in-progress'),
+  isDeleted: z.boolean().default(false).optional(),
+});
