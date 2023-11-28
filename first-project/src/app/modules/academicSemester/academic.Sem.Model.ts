639ee7009c4574fb9c62ffd41013ffaf06a51206
rakib8680 <rakib.cst.1st@gmail.com>
2023-11-28 19:35:15 +0600

    created academic semester interface and model

---
+++ first-project/src/app/modules/academicSemester/academic.Sem.Model.ts
@@ -0,0 +1,52 @@
+import { model, Schema } from 'mongoose';
+import { TAcademicSemester, TMonths } from './academic.Sem.Interface';
+
+const months: TMonths[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const academicSemesterSchema = new Schema<TAcademicSemester>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Date,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+    },
+    startMonth: {
+      type: String,
+      enum: months,
+      required: true,
+    },
+    endMonth: {
+      type: String,
+      enum: months,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const AcademicSemester = model<TAcademicSemester>(
+  'AcademicSemester',
+  academicSemesterSchema,
+);
