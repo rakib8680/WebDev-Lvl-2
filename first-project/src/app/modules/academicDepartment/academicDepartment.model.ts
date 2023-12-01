8eca6d1eb5d02bc8a6e5dc378fbf4d2f7ad344ab
rakib8680 <rakib.cst.1st@gmail.com>
2023-12-01 15:24:47 +0600

    department exist or not validation created

--- first-project/src/app/modules/academicDepartment/academicDepartment.model.ts
+++ first-project/src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -1,26 +1,39 @@
-import { model, Schema } from "mongoose";
-import { TAcademicDepartment } from "./academicDepartment.interface";
-
+import { model, Schema } from 'mongoose';
+import { TAcademicDepartment } from './academicDepartment.interface';
 
 const academicDepartmentSchema = new Schema<TAcademicDepartment>(
-    {
-      name: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      academicFaculty: {
-        type: Schema.Types.ObjectId,
-        ref: 'AcademicFaculty',
-      },
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    {
-      timestamps: true,
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicFaculty',
     },
-  );
+  },
+  {
+    timestamps: true,
+  },
+);
 
 
-  export const AcademicDepartment = model<TAcademicDepartment>(
-    'AcademicDepartment',
-    academicDepartmentSchema,
-  );
+
+// check if department already exist or not
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+  if (isDepartmentExist) {
+    throw new Error('Department already exist');
+  }
+  next();
+});
+
+
+
+export const AcademicDepartment = model<TAcademicDepartment>(
+  'AcademicDepartment',
+  academicDepartmentSchema,
+);
