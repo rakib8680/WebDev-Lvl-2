092cc72b63419a92f3a44b550bfa47d513083066
rakib8680 <rakib.cst.1st@gmail.com>
2023-12-02 16:42:49 +0600

    implemented transaction rollback for delete user/student

--- first-project/src/app/modules/student/student.service.ts
+++ first-project/src/app/modules/student/student.service.ts
@@ -1,3 +1,7 @@
+import httpStatus from 'http-status';
+import mongoose from 'mongoose';
+import AppError from '../../errors/appError';
+import { User } from '../user/user.model';
 import { StudentModel } from './student.model';
 
 
@@ -29,9 +33,45 @@ const getSingleStudentFromDB = async (id: string) => {
   return result;
 };
 
+
+// delete student/user 
 const deleteStudentFromDB = async (id: string) => {
-  const result = await StudentModel.updateOne({ id }, { isDeleted: true });
-  return result;
+  const session =await mongoose.startSession();
+  
+  try {
+  session.startTransaction( )
+
+    // delete student
+  const deletedStudent = await StudentModel.updateOne(
+    { id },
+     { isDeleted: true },
+     {new: true, session}
+     );
+    if(!deletedStudent) throw new AppError(httpStatus.BAD_REQUEST, 'Student not deleted');
+
+
+    // DELETE user
+    const deletedUser = await User.findOneAndUpdate({ id },
+      { isDeleted: true },
+      {new: true, session}
+      );
+     if(!deletedUser) throw new AppError(httpStatus.BAD_REQUEST, 'User not deleted');
+ 
+      await session.commitTransaction();
+      await session.endSession();
+
+  return deletedStudent;
+
+
+}
+
+catch (error) {
+  await session.abortTransaction();
+  await session.endSession();
+}
+
+
+
 };
 
 export const studentServices = {
