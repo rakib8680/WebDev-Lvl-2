cad20c498f81d35f3620e4067525f1669c73a769
rakib8680 <rakib.cst.1st@gmail.com>
2023-12-03 15:38:00 +0600

    custom error message desinged

--- first-project/src/app/middlewares/globalErrorHandler.ts
+++ first-project/src/app/middlewares/globalErrorHandler.ts
@@ -1,20 +1,35 @@
-
-
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler} from 'express';
+import { ZodError } from 'zod';
+
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong';
+
+  type TErrorSource = {
+    path: string | number,
+    message : string
+  }[]
+  const errorSource: TErrorSource = [{
+    path: '',
+    message: 'Something Went Wrong'
+  }];
 
 
-const globalErrorHandler =(err: any, req: Request, res: Response, next: NextFunction) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Something went wrong';
-    return res.status(statusCode).json({
-      success: false,
-      message,
-      error: err,
-    });
-  };
+  if(err instanceof ZodError){
+  statusCode = 400;
+  message = 'Ami Zod Error';
+  }
 
 
-  export default globalErrorHandler;
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    err,
+    errorSource,
+  });
+};
+
+export default globalErrorHandler;
