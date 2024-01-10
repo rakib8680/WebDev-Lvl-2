8119c1d79e02a20f31d7e8da8e605c0c1c5d384d
rakib8680 <rakib.cst.1st@gmail.com>
2024-01-10 16:55:37 +0600

    Project initialized for redux practice

---
+++ Redux-practice/src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <button className="">Increment</button>
+      <h1>React With Redux</h1>
+      <button></button>
+    </div>
+  );
+};
+
+export default App;
