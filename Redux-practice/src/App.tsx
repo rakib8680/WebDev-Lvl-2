bffc3fe4faab90cfa280264e117c6b31cc260509
rakib8680 <rakib.cst.1st@gmail.com>
2024-01-10 17:02:29 +0600

    normal button design added

--- Redux-practice/src/App.tsx
+++ Redux-practice/src/App.tsx
@@ -1,9 +1,15 @@
 const App = () => {
   return (
-    <div>
-      <button className="">Increment</button>
-      <h1>React With Redux</h1>
-      <button></button>
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className=" flex gap-5 border border-purple-800 bg-purple-200 p-10">
+        <button className="bg-rose-600 text-white px-4 py-1 text-2xl font-semibold rounded-sm">
+          Decrement
+        </button>
+        <h1 className="font-bold text-4xl">0</h1>
+        <button className="bg-lime-600 text-white px-4 py-1 text-2xl font-semibold rounded-sm">
+          Increment
+        </button>
+      </div>
     </div>
   );
 };
