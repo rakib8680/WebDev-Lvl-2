8134ecfcf6acd05ebe76086fc192491bfcef6d47
rakib8680 <rakib.cst.1st@gmail.com>
2024-01-11 18:37:37 +0600

    removed old codes

--- Redux-practice/src/redux/features/counterSlice.tsx
+++ Redux-practice/src/redux/features/counterSlice.tsx
@@ -1,37 +1,10 @@
-// // create slice
-// import { PayloadAction, createSlice } from "@reduxjs/toolkit";
-
-// // define initial state
-// type CounterType = {
-//   count: number;
-// };
-// const initialState: CounterType = { count: 0 };
-
-// export const counterSlice = createSlice({
-//   name: "counter",
-//   initialState,
-//   reducers: {
-//     increment: (state) => {
-//       state.count += 1;
-//     },
-//     incrementBy: (state, action: PayloadAction<number>) => {
-//       state.count += action.payload;
-//     },
-//     decrement: (state) => {
-//       state.count -= 1;
-//     },
-//   },
-// });
-
-// // export actions
-// export const { increment, decrement, incrementBy } = counterSlice.actions;
-
-// // export reducer
-// export default counterSlice.reducer;
-
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-const initialState = { count: 0 };
+type TInitialState = {
+  count: number;
+};
+
+const initialState: TInitialState = { count: 0 };
 
 const counterSlice = createSlice({
   name: "counter",
--- Redux-practice/src/redux/store.ts
+++ Redux-practice/src/redux/store.ts
@@ -1,13 +1,4 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import counterReducer from "./features/counterSlice";
-// import logger from "redux-logger";
 
-// export const store = configureStore({
-//   reducer: {
-//     counter: counterReducer,
-//   },
-//   middleware: getDefaultMiddleware =>getDefaultMiddleware().concat(logger)
-// });
 
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counterSlice";
