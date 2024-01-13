fcb37fcc1423e4e5956cb9ff506f524ac5ba6d74
rakib8680 <rakib.cst.1st@gmail.com>
2024-01-13 19:41:35 +0600

    conditionally show no task component implemented

--- L2B2-Todo-starterpack/src/components/todo/TodoContainer.tsx
+++ L2B2-Todo-starterpack/src/components/todo/TodoContainer.tsx
@@ -12,16 +12,17 @@ const TodoContainer = () => {
         <AddTodoModal />
         <TodoFilter />
       </div>
-      <div className="bg-primary-gradient w-full h-full rounded-xl  p-[5px]">
+      {todos.length > 0 ? (
         <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
           {todos.map((item, index) => (
             <TodoCard {...item} key={index} />
           ))}
         </div>
-        {/* <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center rounded-md">
-          <p>There is no task pending</p>{' '}
-        </div> */}
-      </div>
+      ) : (
+        <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center rounded-md">
+          <p>There is no task to show</p>{" "}
+        </div>
+      )}
     </div>
   );
 };
