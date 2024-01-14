976f8d4d8dd3027cee0c16968568899e29a07fa5
rakib8680 <rakib.cst.1st@gmail.com>
2024-01-14 19:01:07 +0600

    done for today

--- L2B2-Todo-with-Redux/src/redux/api/api.ts
+++ L2B2-Todo-with-Redux/src/redux/api/api.ts
@@ -1,13 +1,16 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 export const baseApi = createApi({
-    reducerPath:'baseApi',
-    baseQuery:fetchBaseQuery({baseUrl:'http://localhost:5000'}),
-    endpoints:(builder)=>({
-        addTodo : builder.query({
-            query:()=>{
-                // ... 
-            }
-        })
-    })
-})
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  endpoints: (builder) => ({
+    getTodos: builder.query({
+      query: () => ({
+        url: "/tasks",
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const { useGetTodosQuery } = baseApi.endpoints;
