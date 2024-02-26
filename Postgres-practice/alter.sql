c3febac37355cbb56d2f8037798a6c7b7b7813f0
rakib8680 <rakib.cst.1st@gmail.com>
2024-02-26 17:00:30 +0600

    Add column, insert values, delete row, rename column, change column constraint, add/remove column constraint in person2 table

--- Postgres-practice/alter.sql
+++ Postgres-practice/alter.sql
@@ -1,2 +1,29 @@
 -- Active: 1708873839448@@127.0.0.1@5432@ph
 select * from person2;
+
+-- add column to table
+alter table person2 
+add column email VARCHAR(50) DEFAULT 'ph@gmail.com' not null;
+
+-- insert values 
+insert into person2
+values (9, 'akash', 23, 'akash@gmail.com');
+
+-- delete row 
+delete from person2 where  id = 9;   
+
+
+-- rename column 
+alter table person2
+rename column age to user_age;
+
+
+-- change column constraint 
+alter table person2
+alter column user_name type VARCHAR(100);
+
+
+
+-- add/remove new column constraint 
+alter table person2
+alter column email drop not null;
