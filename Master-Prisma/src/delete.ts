36264d83739a6d5856c28a13ae6ab12a2cbd4cfd
rakib8680 <rakib.cst.1st@gmail.com>
2024-03-08 19:09:56 +0600

    delete data using prisma

---
+++ Master-Prisma/src/delete.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient();
+
+const deletePost = async () =>{
+    const result = await prisma.post.delete({
+        where: {
+            id: 22
+        }
+    })
+
+    console.log(result);
+}
+
+
+deletePost();
