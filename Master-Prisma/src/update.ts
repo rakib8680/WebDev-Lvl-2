c974ba10c1ab172e4ae98e93b854843ae4196680
rakib8680 <rakib.cst.1st@gmail.com>
2024-03-08 19:19:21 +0600

    UPSERT in prisma

--- Master-Prisma/src/update.ts
+++ Master-Prisma/src/update.ts
@@ -12,17 +12,29 @@ const updates = async () => {
     },
   });
 
-
   const multipleUpdate = await prisma.post.updateMany({
     where: {
-      title: 'This is title 2'
+      title: "This is title 2",
     },
     data: {
-      published:true
+      published: true,
     },
   });
 
-//   console.log(multipleUpdate);
+  const upsertData = await prisma.post.upsert({
+    where: {
+      id: 23,
+    },
+    update: {
+      authorName: "rakib",
+    },
+    create: {
+      title: "Title 23",
+      content: "Content 23",
+    },
+  });
+
+  console.log(upsertData);
 };
 
 updates();
