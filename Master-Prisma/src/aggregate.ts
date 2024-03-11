dab06493111871a32a60dfee0d79b79610d7787c
rakib8680 <rakib.cst.1st@gmail.com>
2024-03-11 18:13:47 +0600

    Sum , Count aggregate functions using prisma

--- Master-Prisma/src/aggregate.ts
+++ Master-Prisma/src/aggregate.ts
@@ -1,22 +1,33 @@
 import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 
-const prisma =  new PrismaClient();
+const aggregateFunctions = async () => {
+  //find average
+  const averageAge = await prisma.user.aggregate({
+    _avg: {
+      age: true,
+    },
+  });
 
+  //    find sum of age
+  const sumOfAge = await prisma.user.aggregate({
+    _sum: {
+      age: true,
+    },
+  });
 
+  //    count of age fields
+  const count = await prisma.user.aggregate({
+    _count: {
+      age: true,
+    },
+  });
 
-const aggregateFunctions = async ()=>{
+  //    count total records
+  const countData = await prisma.user.count();
 
-   const averageAge = await prisma.user.aggregate({
-    _avg:{
-        age:true
-    }
-   })
-
-
-   console.log(averageAge._avg.age);
-
-}
-
+  console.log(countData);
+};
 
 aggregateFunctions();
