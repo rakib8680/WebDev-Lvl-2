4c86cfcd958c0fa13f20fb60a07476e7d1eb6e22
rakib8680 <rakib.cst.1st@gmail.com>
2024-03-12 12:59:21 +0600

    Raw query in prisma

---
+++ Master-Prisma/src/rawQuery.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const rawQuery = async () => {
+  // get all posts
+  const posts = await prisma.$queryRaw`select * from posts`;
+
+  // delete users table data
+  await prisma.$queryRaw`truncate table users cascade`;
+
+//   console.log(posts);
+};
+
+// rawQuery();
