d20864208dbbdfefcb7f9dc10a5cb747dd38dd00
rakib8680 <rakib.cst.1st@gmail.com>
2024-04-21 17:21:23 +0600

    Explored Font Optimization

--- Next Js Projects/my-next-app-2/src/app/layout.tsx
+++ Next Js Projects/my-next-app-2/src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="light">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="light" className={roboto.className}>
+      <body>{children}</body>
     </html>
   );
 }
