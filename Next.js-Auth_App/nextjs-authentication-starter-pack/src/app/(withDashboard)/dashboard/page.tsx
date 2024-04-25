e5da311c7ee53283935c002225289727e7e841f2
rakib8680 <rakib.cst.1st@gmail.com>
2024-04-25 19:29:43 +0600

    Implemented Github login and show user info on Dashboard Page

--- Next.js-Auth_App/nextjs-authentication-starter-pack/src/app/(withDashboard)/dashboard/page.tsx
+++ Next.js-Auth_App/nextjs-authentication-starter-pack/src/app/(withDashboard)/dashboard/page.tsx
@@ -1,7 +1,26 @@
-const DashboardPage = () => {
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+
   return (
-    <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
+    <div className="space-y-5">
+      <h1 className="text-4xl text-center mt-10">
+        Welcome {session?.user?.name}
+      </h1>
+      <h1 className="text-4xl text-center mt-10">
+        Email {session?.user?.email}
+      </h1>
+      <Image
+        className="mx-auto mask mask-squircle"
+        src={session?.user?.image as string}
+        alt="user_image"
+        width={100}
+        height={100}
+      />
     </div>
   );
 };
