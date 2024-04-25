50234e5427a465fcf54b67eca99a724bd7330db2
rakib8680 <rakib.cst.1st@gmail.com>
2024-04-25 21:10:15 +0600

    logout Functionality Implemented

--- Next.js-Auth_App/nextjs-authentication-starter-pack/src/app/(withDashboard)/dashboard/page.tsx
+++ Next.js-Auth_App/nextjs-authentication-starter-pack/src/app/(withDashboard)/dashboard/page.tsx
@@ -4,24 +4,30 @@ import Image from "next/image";
 
 const DashboardPage = async () => {
   const session = await getServerSession(authOptions);
-  console.log(session);
+  // console.log(session);
 
   return (
-    <div className="space-y-5">
-      <h1 className="text-4xl text-center mt-10">
-        Welcome {session?.user?.name}
-      </h1>
-      <h1 className="text-4xl text-center mt-10">
-        Email {session?.user?.email}
-      </h1>
-      <Image
-        className="mx-auto mask mask-squircle"
-        src={session?.user?.image as string}
-        alt="user_image"
-        width={100}
-        height={100}
-      />
-    </div>
+    <>
+      {session?.user ? (
+        <div className="space-y-5">
+          <h1 className="text-4xl text-center mt-10">
+            Welcome {session?.user?.name}
+          </h1>
+          <h1 className="text-4xl text-center mt-10">
+            Email {session?.user?.email}
+          </h1>
+          <Image
+            className="mx-auto mask mask-squircle"
+            src={session?.user?.image as string}
+            alt="user_image"
+            width={100}
+            height={100}
+          />
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
